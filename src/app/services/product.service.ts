eac0bd57ea0b289fb34bfff7fd554a9023dc3db4
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-03 15:56:25 +0200

    Get all products impl on ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  productsCollection;
+  products: Observable<Product[]>;
+  productDoc;
+
+  constructor(
+    private db: AngularFirestore
+  ) { 
+    this.products = this.db.collection('products').valueChanges();
+  }
+
+  getProduct() {
+    return this.products;
+  }
+
 }
